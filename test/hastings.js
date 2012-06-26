ee2a2fdb4ddaae1e8a83f0b9fd7aa443f4e728fd
Michael Brooks <michael@michaelbrooks.ca>
2012-06-26 16:32:39 -0700

    Add basic hastings init

--- test/hastings.js
+++ test/hastings.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var hastings = require('./../lib/hastings');
+var basepath = require('path').resolve('.');
 
 describe('hastings', function() {
     it('should exist', function() {
@@ -9,6 +10,27 @@ describe('hastings', function() {
     describe('init', function() {
         it('should exist', function() {
             hastings.should.have.property('init');
+        });
+
+        describe('path', function() {
+            it('should be optional', function() {
+                var path = hastings.init();
+                path.should.equal(basepath + '/doc');
+            });
+
+            it('should be used when provided', function() {
+                var path = hastings.init('docs');
+                path.should.equal(basepath + '/docs');
+            });
+
+            it('should handle ./ as ./doc', function() {
+                var path = hastings.init('.');
+                path.should.equal(basepath + '/doc')
+            });
+        });
+
+        describe('output', function() {
+            it('should output ./doc/index.md');
         });
     });
 });
